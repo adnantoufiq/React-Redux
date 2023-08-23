ee3e3d3bf1d88ee1dc40e159701cfe776270d41e
adnantoufiq <adnantoufiq69@gmail.com>
2023-08-23 19:16:09 +0600

    created user and counter reducer

---
+++ React-Redux/consts.js
@@ -0,0 +1,15 @@
+const  INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+const RESET = "RESET"
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE"
+const ADD_USER = "ADD_USER" 
+
+
+
+module.exports ={
+    INCREMENT,
+    DECREMENT,
+    RESET,
+    INCREMENT_BY_VALUE,
+    ADD_USER
+}
